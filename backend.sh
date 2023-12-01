426f8386158650623e8f41dd42d3126041f3db9e
prasanna <prasannadoradla27@gmail.com>
2023-12-01 19:32:41 +0530

    save code

---
+++ backend.sh
@@ -0,0 +1,19 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
+useradd expense
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+cd /app
+npm install
+dnf install mysql -y
+mysql -h mysql.pdevops1126.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
+
+
