57b018447149a2c5bd89729a186b18b06ba03406
prasanna <prasannadoradla27@gmail.com>
2023-12-04 08:19:20 +0530

    save code

--- frontend.sh
+++ frontend.sh
@@ -11,22 +11,22 @@ cp expense.conf  /etc/nginx/default.d/expense.conf &>>${log_file}
 echo $?
 
 echo -e "${color} enable nginx service \e[0m"
-systemctl enable nginx &>>$log_file
+systemctl enable nginx &>>${log_file}
 echo $?
 echo -e "${color} starting nginx service \e[0m"
-systemctl start nginx &>>$log_file
+systemctl start nginx &>>${log_file}
 echo $?
 echo -e "${color} remove default files in nginx \e[0m"
-rm -rf /usr/share/nginx/html/* &>>$log_file
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 echo $?
 
 echo -e "${color} download software \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 echo $?
 echo -e "${color} extract software \e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$log_file
+unzip /tmp/frontend.zip &>>${log_file}
 echo $?
 
-systemctl restart nginx &>>$log_file
+systemctl restart nginx &>>${log_file}
 echo $?
