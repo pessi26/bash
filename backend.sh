7e4173cbc57a0ea0a495d2f09a9c6a1a1e44afa4
prasanna <prasannadoradla27@gmail.com>
2023-12-15 20:33:37 +0530

    save code

--- backend.sh
+++ backend.sh
@@ -14,11 +14,11 @@ echo -e ${color} "install node js utilities \e[0m"
 dnf install nodejs -y &>>${log_file}
 echo $?
 
-echo -e ${color} "copy backend service file" \e[0m
+echo -e ${color} "copy backend service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>${log_file}
 echo $?
 
-echo -e ${color} "enable backend service" \e[0m
+echo -e ${color} "enable backend service \e[0m"
 systemctl daemon-reload &>>${log_file}
 echo $?
 systemctl enable backend &>>${log_file}
@@ -26,26 +26,26 @@ echo $?
 systemctl start backend &>>${log_file}
 echo $?
 
-echo -e ${color} "add demon user " \e[0m
+echo -e ${color} "add demon user  \e[0m "
 useradd expense &>>${log_file}
 echo $?
 
-echo -e ${color} "download backend code " \e[0m
+echo -e ${color} "download backend code \e[0m "
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>${log_file}
 echo $?
 
-echo -e ${color} "unzip the download " \e[0m
+echo -e ${color} "unzip the download \e[0m "
 cd /app &>>${log_file}
 unzip /tmp/backend.zip &>>${log_file}
 echo $?
 
-echo -e ${color} "install npm " \e[0m
+echo -e ${color} "install npm \e[0m "
 cd /app
 npm install &>>${log_file}
 echo $?
 
-echo -e ${color} "install mysql " \e[0m
+echo -e ${color} "install mysql  \e[0m"
 dnf install mysql -y &>>${log_file}
 mysql -h mysql.pdevops1126.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>${log_file}
 echo $?
